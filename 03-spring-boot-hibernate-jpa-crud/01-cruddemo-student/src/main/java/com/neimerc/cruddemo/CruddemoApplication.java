cc5ce3e2d7cfc2dbcd622bf42e4b6d4fdf88d77e
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-11-07 16:51:32 +0100

    03 - Hibernate/JPA - CommandLineRunner

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/neimerc/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/neimerc/cruddemo/CruddemoApplication.java
@@ -1,7 +1,9 @@
 package com.neimerc.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -10,4 +12,13 @@ public class CruddemoApplication {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
+	// Creación de una app de línea de comandos.
+	// CommandLineRunner es del Framework Spring Boot.
+	// Se ejecuta tras haberse cargado los Beans de Spring, por lo que podemos usarlos aquí.
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World!");
+		};
+	}
 }
