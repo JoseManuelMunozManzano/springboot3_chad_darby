ea33bbf20dc8edb3e080d112186d1284aef76cc9
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-11-11 21:37:09 +0100

    04 - REST Crud - Refactor y uso de @PostConstruct

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/neimerc/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/neimerc/demo/rest/StudentRestController.java
@@ -9,20 +9,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.neimerc.demo.entity.Student;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-  
-  // definir un endpoint para "/students" - devuelve una lista de estudiantes
-  @GetMapping("/students")
-  public List<Student> getStudents() {
-    // Por ahora, datos hardcodeados para hacerlo sencillo
-    List<Student> theStudents = new ArrayList<>();
+
+  private List<Student> theStudents;
+
+  // definir @PostConstruct para cargar la data de student ... solo una vez!
+  // Por ahora, datos hardcodeados para hacerlo sencillo
+  @PostConstruct
+  public void loadData() {
+    theStudents = new ArrayList<>();
 
     theStudents.add(new Student("Adriana", "López"));
     theStudents.add(new Student("José M.", "Muñoz"));
     theStudents.add(new Student("Tania", "Pérez"));
-
+  }
+  
+  // definir un endpoint para "/students" - devuelve una lista de estudiantes
+  @GetMapping("/students")
+  public List<Student> getStudents() {
     // Jackson convierte automáticamente nuesta lista de Student a un array JSON
     return theStudents;
   }
