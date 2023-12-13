f1b175b51ac258f4b2f96400eb393bf11db23fab
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-12-13 20:14:13 +0100

    08 - MVC Security - Autenticación JDBC - Texto plano

--- 08-spring-boot-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/jmmunoz/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/jmmunoz/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -1,38 +1,28 @@
 package com.jmmunoz.springboot.demosecurity.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+// import org.springframework.security.core.userdetails.User;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
-  
+
+
+  // añadir soporte para JDBC ... no más usuarios hardcode !!
+  // Inyectamos un DataSource que es auto-configurado por Spring Boot
   @Bean
-  public InMemoryUserDetailsManager userDetailsManager() {
-    
-    UserDetails john = User.builder()
-        .username("john")
-        .password("{noop}test123")
-        .roles("EMPLOYEE")
-        .build();
-
-    UserDetails mary = User.builder()
-        .username("mary")
-        .password("{noop}test123")
-        .roles("EMPLOYEE", "MANAGER")
-        .build();
-
-    UserDetails susan = User.builder()
-        .username("susan")
-        .password("{noop}test123")
-        .roles("EMPLOYEE", "MANAGER", "ADMIN")
-        .build();
-
-    return new InMemoryUserDetailsManager(john, mary, susan);
+  public UserDetailsManager userDetailsManager(DataSource dataSource) {
+    // Indica a Spring Security que use autenticación JDBC con nuestro data source.
+    // Spring Security buscará las tablas con nombre por defecto users y authorities
+    return new JdbcUserDetailsManager(dataSource);
   }
 
   // Configuración para que Spring Security referencia a nuestro formulario de login personalizado.
@@ -56,4 +46,30 @@ public class DemoSecurityConfig {
 
     return http.build();
   }
+
+/*
+  @Bean
+  public InMemoryUserDetailsManager userDetailsManager() {
+    
+    UserDetails john = User.builder()
+        .username("john")
+        .password("{noop}test123")
+        .roles("EMPLOYEE")
+        .build();
+
+    UserDetails mary = User.builder()
+        .username("mary")
+        .password("{noop}test123")
+        .roles("EMPLOYEE", "MANAGER")
+        .build();
+
+    UserDetails susan = User.builder()
+        .username("susan")
+        .password("{noop}test123")
+        .roles("EMPLOYEE", "MANAGER", "ADMIN")
+        .build();
+
+    return new InMemoryUserDetailsManager(john, mary, susan);
+  } 
+*/
 }
