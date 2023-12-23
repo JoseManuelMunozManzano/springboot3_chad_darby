a7f5ae5c2116615d795ec8bcfc421ebeb64a0dbd
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-12-23 07:13:51 +0100

    09 - Advanced JPA - Create CommandLineRunner

--- 09-spring-boot-jpa-hibernate-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/jmmunoz/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-hibernate-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/jmmunoz/cruddemo/CruddemoApplication.java
@@ -1,7 +1,9 @@
 package com.jmmunoz.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -10,4 +12,13 @@ public class CruddemoApplication {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
+	// Definimos el bean para crear una app de línea de comandos.
+	// CommandLineRunner viene de Spring Boot y este método se ejecuta tras haberse cargado los Spring Beans.
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
 }
