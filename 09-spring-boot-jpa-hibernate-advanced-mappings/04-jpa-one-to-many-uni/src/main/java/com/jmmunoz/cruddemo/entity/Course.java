111b5cfd0eb3e2348934bba85a55745e2ed0945b
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-12-24 19:04:39 +0100

    09 - Advanced JPA - Refactor Course Entity

--- 09-spring-boot-jpa-hibernate-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/jmmunoz/cruddemo/entity/Course.java
+++ 09-spring-boot-jpa-hibernate-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/jmmunoz/cruddemo/entity/Course.java
@@ -1,13 +1,18 @@
 package com.jmmunoz.cruddemo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -28,6 +33,11 @@ public class Course {
   @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
   @JoinColumn(name = "instructor_id")
   private Instructor instructor;
+
+  // Referencia al id del review. Así, sabremos como encontrar el course al que pertenece una review.
+  @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  @JoinColumn(name = "course_id")
+  private List<Review> reviews;
   
   // define constructors
   public Course() {
@@ -61,10 +71,31 @@ public class Course {
   public void setInstructor(Instructor instructor) {
     this.instructor = instructor;
   }
+
+  public List<Review> getReviews() {
+    return reviews;
+  }
+
+  public void setReviews(List<Review> reviews) {
+    this.reviews = reviews;
+  }
+
+  // add a convenience method
+  public void addReview(Review theReview) {
+
+    if (reviews == null) {
+      reviews = new ArrayList<>();
+    }
+
+    reviews.add(theReview);
+  }
+  
   // define toString
   @Override
   public String toString() {
     return "Course [id=" + id + ", title=" + title + "]";
   }
+  
+
 
 }
